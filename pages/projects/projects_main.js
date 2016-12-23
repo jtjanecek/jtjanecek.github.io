0f85b6717d4510999e0e32e34c4d8f894aeccd99
jtjanecek <janecektyler@gmail.com>
2016-12-22 17:00:23 -0800

    Added links to projects

--- pages/projects/projects_main.js
+++ pages/projects/projects_main.js
@@ -6,6 +6,23 @@ function main() {
     $("body").fadeIn(fadeInTime);
 
 
+    $("#dat_boi_td").click(function(eventHandler){
+        redirect = "./dat_boi/dodge_meme.html";
+        eventHandler.preventDefault();
+        $('body').fadeOut(fadeOutTime, function(){
+            document.location.href = redirect;
+          });
+    });
+
+    $("#nerd_purity_td").click(function(eventHandler){
+        redirect = "./NerdPurity/nerd_purity.html";
+        eventHandler.preventDefault();
+        $('body').fadeOut(fadeOutTime, function(){
+            document.location.href = redirect;
+          });
+    });
+
+
 };
 
 
